36fb08139ea4a50dd2a0c656eec1e92064122312
Ziad Ismael <117987711+ziadismael@users.noreply.github.com>
2023-12-20 00:42:56 +0200

    Create GameManager.cpp
    Initial implementations given for the original 3x3 tic tac toe.

---
+++ Console Based Version/src/GameManager.cpp
@@ -0,0 +1,38 @@
+// Class definition for XO_GameManager class
+// Author:  Mohammad El-Ramly
+// Date:    10/10/2022
+// Version: 1
+
+#include <iostream>
+#include"../include/BoardGame_Classes.hpp"
+using namespace std;
+
+GameManager::GameManager(Board* bPtr, Player* playerPtr[2]) {
+    boardPtr = bPtr;
+    players[0] = playerPtr[0];
+    players[1] = playerPtr[1];
+}
+
+void GameManager::run() {
+    int x, y;
+
+    boardPtr->display_board();
+
+    while (!boardPtr->game_is_over()) {
+        for (int i : {0, 1}) {
+            players[i]->get_move(x, y);
+            while (!boardPtr->update_board(x, y, players[i]->get_symbol())) {
+                players[i]->get_move(x, y);
+            }
+            boardPtr->display_board();
+            if (boardPtr->is_winner()) {
+                cout << players[i]->to_string() << " wins\n";
+                return;
+            }
+            if (boardPtr->is_draw()) {
+                cout << "Draw!\n";
+                return;
+            }
+        }
+    }
+}
